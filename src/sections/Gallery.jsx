9850c3c875287d7968324287d251c0247d132171
Zet <zettiemayuga@gmail.com>
2024-04-20 02:03:45 +0800

    fix: margin

--- src/sections/Gallery.jsx
+++ src/sections/Gallery.jsx
@@ -7,7 +7,7 @@ const Gallery = (props) => {
         <div id='gallery' className="lg:py-24 py-16 scroll-m-[5rem]">
             <img src={props.data.assets.leaves1} className="absolute left-20 -mt-1 hidden lg:block" />
             <img src={props.data.assets.leaves2} className="absolute right-20 mt-72 scale-y-[-1] hidden lg:block" />
-            <div className='font-sans text-brown text-5xl text-center lg:mb-12 md:mb-8 mb-1'>{props.data.section[2].name}</div>
+            <div className='font-sans text-brown text-5xl text-center lg:mb-12 mb-8'>{props.data.section[2].name}</div>
             <PrenupCarousel {...props}/>
             <WeddingCarousel {...props}/>
         </div>
